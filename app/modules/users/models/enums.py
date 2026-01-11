d92cc37fa7a1b304e29023c3540ac2133b9cb58e
Oleksii Drenov <leshadrenev@gmail.com>
2026-01-11 22:39:37 +0200

    feat: add user role and gender enums

---
+++ app/modules/users/models/enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class UserGroupEnum(Enum):
+    USER = "USER"
+    MODERATOR = "MODERATOR"
+    ADMIN = "ADMIN"
+
+
+class GenderEnum(Enum):
+    MAN = "MAN"
+    WOMAN = "WOMAN"
