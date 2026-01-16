ee93abd9e71ed3c338ac3ce066451b1749178ee9
Oleksii Drenov <leshadrenev@gmail.com>
2026-01-17 00:24:58 +0200

    feat(exceptions): add security and email exceptions

---
+++ app/exceptions/exceptions.py
@@ -0,0 +1,47 @@
+class BaseSecurityError(Exception):
+    """Base class for all security-related errors."""
+
+    def __init__(self, message=None):
+        if message is None:
+            message = "A security error occurred."
+        super().__init__(message)
+
+
+class TokenExpiredError(BaseSecurityError):
+    """Raised when a token has expired."""
+
+    def __init__(self, message="Token has expired."):
+        super().__init__(message)
+
+
+class InvalidTokenError(BaseSecurityError):
+    """Raised when a token is invalid."""
+
+    def __init__(self, message="Invalid token."):
+        super().__init__(message)
+
+
+class UserNotActiveError(BaseSecurityError):
+    """Raised when user account is not active."""
+
+    def __init__(self, message: str = "User account is not active."):
+        super().__init__(message)
+
+
+class InvalidCredentialsError(BaseSecurityError):
+    """Raised when user credentials are invalid."""
+
+    def __init__(self, message: str = "Invalid credentials."):
+        super().__init__(message)
+
+
+class TokenNotFoundError(BaseSecurityError):
+    """Raised when a token is not found in storage."""
+
+    def __init__(self, message: str = "Token not found."):
+        super().__init__(message)
+
+
+class BaseEmailError(Exception):
+    """Base class for all exceptions raised by email notification module."""
+    pass
