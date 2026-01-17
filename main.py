9a3d6e98c1cf93729d97d2f0b8c830cbbd2fcd03
Oleksii Drenov <leshadrenev@gmail.com>
2026-01-17 14:06:48 +0200

    feat: include auth router in main application

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 
+from app.modules.users.routers.auth import auth_router
+
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"message": "Hello, World!"}
+
+
+app.include_router(auth_router)
