6818dc1e186cc12d6db5121ab9145109af565ccb
Oleksii Drenov <leshadrenev@gmail.com>
2026-01-18 20:16:27 +0200

    feat: add strong password validation to reset password schema

--- app/modules/users/schemas/password_schema.py
+++ app/modules/users/schemas/password_schema.py
@@ -5,18 +5,18 @@ from app.utils.security import validate_strong_password
 
 class PasswordResetRequestSchema(BaseModel):
     email: EmailStr
-    token: str
-    password: str
-
-    @field_validator("password")
-    @classmethod
-    def validate_password(cls, value):
-        return validate_strong_password(value)
 
     model_config = {"extra": "forbid"}
 
 
 class PasswordResetCompleteRequestSchema(BaseModel):
     email: EmailStr
+    token: str
+    new_password: str
+
+    @field_validator("new_password")
+    @classmethod
+    def validate_password(cls, value):
+        return validate_strong_password(value)
 
     model_config = {"extra": "forbid"}
