58b003e9d4e40216c86907855d84dc2edef0e947
Oleksii Drenov <leshadrenev@gmail.com>
2026-01-28 16:09:27 +0200

    feat(movies): add examples for movie item and movie list response schema

--- app/modules/movies/schemas/examples/movies_schema_examples.py
+++ app/modules/movies/schemas/examples/movies_schema_examples.py
@@ -17,3 +17,21 @@ certification_schema_example = {
     "id": 1,
     "name": "PG-13"
 }
+
+movie_list_item_example = {
+    "id": 1,
+    "name": "Inception",
+    "year": 2010,
+    "imdb": 8.8,
+    "price": "9.99"
+}
+
+movie_list_response_schema_example = {
+    "movies": [
+        movie_list_item_example
+    ],
+    "prev_page": "/theater/movies/?page=1&per_page=1",
+    "next_page": "/theater/movies/?page=3&per_page=1",
+    "total_pages": 9933,
+    "total_items": 9933
+}
