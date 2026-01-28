447f3bfffc0b0025030a53baa70ead138b47167b
Oleksii Drenov <leshadrenev@gmail.com>
2026-01-28 16:07:55 +0200

    feat(movies): implement MovieListResponseSchema with pagination support

--- app/modules/movies/schemas/movie_schema.py
+++ app/modules/movies/schemas/movie_schema.py
@@ -1,9 +1,11 @@
 import decimal
+from decimal import Decimal
+from typing import List, Optional
 
 from pydantic import BaseModel, Field
 
 from app.modules.movies.schemas.examples.movies_schema_examples import director_schema_example, star_schema_example, \
-    genre_schema_example, certification_schema_example
+    genre_schema_example, certification_schema_example, movie_list_response_schema_example, movie_list_item_example
 
 
 class GenreSchema(BaseModel):
@@ -73,4 +75,46 @@ class MovieBaseSchema(BaseModel):
 
     model_config = {
         "from_attributes": True
+    }
+
+
+class MovieListItemSchema(BaseModel):
+    id: int
+    name: str
+    year: int
+    imdb: float
+    price: Decimal
+
+    model_config = {
+        "from_attributes": True,
+        "json_schema_extra": {
+            "examples": [
+                movie_list_item_example
+            ]
+        }
+    }
+
+
+class MovieDetailSchema(MovieListItemSchema):
+    description: str
+    genres: list[GenreSchema]
+    stars: list[StarSchema]
+    directors: list[DirectorSchema]
+    certification: CertificationSchema | None
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieListItemSchema]
+    prev_page: Optional[str]
+    next_page: Optional[str]
+    total_pages: int
+    total_items: int
+
+    model_config = {
+        "from_attributes": True,
+        "json_schema_extra": {
+            "examples": [
+                movie_list_response_schema_example
+            ]
+        }
     }
